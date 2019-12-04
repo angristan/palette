1b37a3fa236fbb22ebcc57cf6f3c796ebea8e8b8
angristan <angristan@pm.me>
2019-12-04 12:20:27 +0900

    Add heroku support

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
+import logging
+import os
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+if app.config['LOG_TO_STDOUT']:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    app.logger.addHandler(stream_handler)
+else:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log',
+                                        maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
 bootstrap = Bootstrap(app)
 
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOADS_DEFAULT_DEST = os.environ.get('UPLOADS_DEFAULT_DEST') or '/tmp'
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
