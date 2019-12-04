a5a1c4410915f90aa7eaeb9d2c6fbdb3df740799
angristan <angristan@pm.me>
2019-12-04 11:23:12 +0900

    Add bootsrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+bootstrap = Bootstrap(app)
 
 images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
