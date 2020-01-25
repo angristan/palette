bda6d33e984457f14ab6d751cd94d7c7ad92641d
angristan <angristan@pm.me>
2020-01-25 22:17:25 +0100

    Remove all db-related code

--- api.py
+++ api.py
@@ -1,7 +1,1 @@
-from app import app, db
-from app.models import Color
-
-# Will make these objects available when running "flask shell"
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Color': Color}
+from app import app
