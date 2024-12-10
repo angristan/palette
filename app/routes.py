95d596876a4ca09786a3bf3de8b7c9c941d30dc6
Stanislas Lange <git@slange.me>
2024-12-10 19:31:32 +0100

    Refactor handlers
    Fix #20

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,14 @@
 import os
-
-from flask import render_template, request
-from flask.json import jsonify
-from webcolors import hex_to_rgb
+from flask import render_template, request, jsonify
 from werkzeug.utils import secure_filename
+from webcolors import hex_to_rgb
 
 from app import app
 from app.forms import UploadForm
 from app.utils.kmeans import Kmeans
 from app.utils.knn import Knn
 
-ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 
 def allowed_file(filename):
@@ -26,45 +24,44 @@ def index():
 @app.route("/get_palette", methods=["POST"])
 def palette():
     if "clusters" not in request.form:
-        resp = jsonify({"message": "No number of clusters specified"})
-        resp.status_code = 400
-        return resp
-    if "image" not in request.files:
-        resp = jsonify({"message": "No file part in the request"})
-        resp.status_code = 400
-        return resp
+        return jsonify({"message": "No number of clusters specified"}), 400
+    if "image" not in request.files or request.files["image"].filename == "":
+        return jsonify({"message": "No file selected for uploading"}), 400
+
     file = request.files["image"]
-    if file.filename == "":
-        resp = jsonify({"message": "No file selected for uploading"})
-        resp.status_code = 400
-        return resp
-    if file and allowed_file(file.filename):
+
+    if allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config["UPLOADS_DEFAULT_DEST"], filename)
         file.save(filepath)
-        cluster_n = int(request.form.get("clusters"))
+
+        try:
+            cluster_n = int(request.form.get("clusters"))
+        except ValueError:
+            os.remove(filepath)
+            return jsonify({"message": "Invalid number of clusters"}), 400
+
         kmeans = Kmeans(filepath, cluster_n)
         color_hex = kmeans.get_tints()
-        print(color_hex)
-        color_names = []
 
+        color_names = []
+        knn = Knn()
         for color in color_hex:
             r, g, b = hex_to_rgb(color)
-            name = Knn().get_color_name(r, g, b)
+            name = knn.get_color_name(r, g, b)
             color_names.append(name)
 
-        print(color_names)
+        os.remove(filepath)
 
-        resp = jsonify(
-            {
-                "message": "File successfully uploaded",
-                "colors": color_hex,
-                "names": color_names,
-            }
+        return (
+            jsonify(
+                {
+                    "message": "File successfully uploaded",
+                    "colors": color_hex,
+                    "names": color_names,
+                }
+            ),
+            201,
         )
-        resp.status_code = 201
-        return resp
     else:
-        resp = jsonify({"message": "Allowed file types are png, jpg, jpeg"})
-        resp.status_code = 400
-        return resp
+        return jsonify({"message": "Allowed file types are png, jpg, jpeg"}), 400
